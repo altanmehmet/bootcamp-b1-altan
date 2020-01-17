8356fef80ac41f78abd8c0d7b9c6cd074958c157
khyseis <mehmetturkmen200@hotmail.com>
2020-01-17 16:53:10 +0300

    Model Added

---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_bootcamp_b1_altan.Models
+{
+    public class Book
+{
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [Required,MaxLength(200)]
+        public string Name { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        [Required]
+        public DateTime ReleasedAt { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
